c61664c65a57e8723f5a058f308270de47e00fe4
nazruliul <airbaptis@gmail.com>
2026-07-27 09:28:43 +0700

    lanjut_CRUD_siswa

---
+++ database/migrations/2026_07_27_022129_create_siswas_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('siswas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('siswas');
+    }
+};
+public function up(): void
+{
+    Schema::create('siswas', function (Blueprint $table) {
+        $table->id();
+        $table->string('nis')->unique();
+        $table->string('nama');
+        $table->string('kelas');
+        $table->string('jurusan')->nullable();
+        $table->string('jenis_kelamin');
+        $table->string('alamat')->nullable();
+        $table->timestamps();
+    });
+}
